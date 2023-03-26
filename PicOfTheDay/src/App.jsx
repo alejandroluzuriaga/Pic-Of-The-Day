2c50ac8b76353b5c42687d2effd1aeb3299f00ca
alejandroluzuriaga <luzuriagalejandro@gmail.com>
2023-03-26 01:52:26 +0100

    Finished project

--- PicOfTheDay/src/App.jsx
+++ PicOfTheDay/src/App.jsx
@@ -6,10 +6,13 @@ import TextoExpansible from './components/TextoExpansible'
 import Loading from './components/Loading';
 
 function App() {
-  const today = new Date(Date.now()).toISOString().slice(0, 10);
+  const todayEurope = new Date(Date.now())
+  todayEurope.setHours(todayEurope.getHours()-8)
+  const today = todayEurope.toISOString().slice(0, 10); //La fecha de hoy es realmente la hora actual menos 8 horas para no tener problemas con la API entre las 00:00 y las 08:00.
   const [actualDate, setDate] = useState(today)
   const [data, setData] = useState(null)
   const [roverData, setRoverData] = useState(null)
+  const [actualComponent, setActualComponent] = useState("nasa")
 
   const NASA_URL = "https://api.nasa.gov/";
   const NASA_API_KEY = "3tct8donAIi6LgIwIQNht08wQJhCYMcZcvjSugZh";
@@ -23,39 +26,41 @@ function App() {
   };
 
   const handleNewSelectOption = (e) => {
-      setRoverData(e.target.value);
+    if(e.target.value == "mars-rover-photo"){
+      setActualComponent("rover");
+
+    }else{
+      setActualComponent("nasa");
+    } 
     }
 
   useEffect(()=>{
-    const APICallAPOD = () =>{
-      axios.get(`${NASA_URL}planetary/apod?date=${actualDate}&api_key=${NASA_API_KEY}`)
-      .then(res =>{
-        setData(res.data)
-      }).catch(err =>{
-        throw new Error('Error')
-      })
+    if (actualComponent === "nasa"){
+        const APICallAPOD = () =>{
+          axios.get(`${NASA_URL}planetary/apod?date=${actualDate}&api_key=${NASA_API_KEY}`)
+          .then(res =>{
+            setData(res.data)
+            setActualComponent('nasa')
+          }).catch(err =>{
+            throw new Error('Error')
+          })
+        }
+        APICallAPOD() 
+    } else if(actualComponent === "rover"){
+        const APICallRovers = () =>{
+          axios.get(`${NASA_URL}mars-photos/api/v1/rovers/curiosity/photos?earth_date=${actualDate}&api_key=${NASA_API_KEY}`)
+          .then(res =>{
+            setRoverData(res.data.photos[0])
+          }).catch(err =>{
+            console.log(err)
+          })
+        }
+        APICallRovers()
     }
-    // APICallAPOD() 
-  }, [actualDate])
+  }, [actualDate, actualComponent])
 
   useEffect(()=>{
-    if(roverData == "mars-rover-photo"){
-      const APICallRovers = () =>{
-        console.log("Llamando API ROVERS")
-        // axios.get(`${NASA_URL}mars-photos/api/v1/rovers/curiosity/photos?earth_date=${actualDate}&api_key=${NASA_API_KEY}`)
-        // .then(res =>{
-        //   // setRoverData(res.data)
-        //   console.log(res)
-        // }).catch(err =>{
-        //   throw new Error('Error')
-        // })
-      }
-      APICallRovers()
-    }
-  }, [roverData])
-
-  useEffect(()=>{
-    if (data){
+    if (data || roverData){
       document.body.style.height = "100%";
     }else{
       document.body.style.height = "100vh";
@@ -65,16 +70,30 @@ function App() {
   return (
     <div className="App">
         <div className="image">
-            <img className='logo_nasa' src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/800px-NASA_logo.svg.png" alt="Image of the day"></img>
+            <img 
+              className='logo_nasa' 
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/800px-NASA_logo.svg.png" 
+              alt="Image of the day">
+            </img>
         </div>
-        {/* <Loading/> */}
-        {data ? (
-        <Figure 
-        actualDate={actualDate} 
-        data={data} 
-        handleNewDate={handleNewDate}
-        handleNewSelectOption={handleNewSelectOption}
-        />) : (<Loading/>)}
+        {data && actualComponent === 'nasa' ? (
+          <Figure 
+            actualDate={actualDate} 
+            data={data} 
+            handleNewDate={handleNewDate}
+            handleNewSelectOption={handleNewSelectOption}
+          />
+        ): 
+        actualComponent == 'rover' ?(
+          <Figure 
+            actualDate={actualDate}
+            roverData={roverData} 
+            handleNewDate={handleNewDate}
+            handleNewSelectOption={handleNewSelectOption}
+          />
+        ) : (
+          <Loading/>
+        )}
       </div>
   )
 }
--- PicOfTheDay/src/components/Figure.jsx
+++ PicOfTheDay/src/components/Figure.jsx
@@ -12,10 +12,40 @@ const handleNewDate = (e) => {
   };
 
 const handleNewSelectOption = (e) => {
-    props.handleNewSelectOption(e);
+    props.handleNewSelectOption();
   }
 
-const Figure = ({actualDate, data, handleNewDate, handleNewSelectOption}) => {
+const Figure = ({actualDate, data, handleNewDate, handleNewSelectOption, roverData}) => {
+  if (roverData){
+    let textAUX = `Landing Date: ${roverData.rover.landing_date}`
+    return (
+      <div>
+        <div className="title">
+            <h1>Astronomic image of the day</h1>
+        </div>
+        <div className="info_container">
+          <div className="select_container">
+          <select className='select' onChange={handleNewSelectOption}>
+            <option value="nasa" defaultValue>NASA</option>
+            <option value="mars-rover-photo">Mars Rover Photo</option>
+          </select>
+          </div>
+            <div className='input'>
+                <label className='date_text'>Pick a date: </label>
+                <input type="date" id="date" name="date" min="1995-06-20" max={today} value={actualDate} onChange={handleNewDate} />
+            </div>
+            <div className="image_info">
+                <h2>Pic of the day: {roverData.earth_date}</h2>
+                <p className='author'>Camera: {roverData.camera.name}</p>
+            </div>
+            <div className="element-image">
+                <img className='nasa_photo' src={roverData.img_src} alt="Rover - Image of the day"></img>
+            </div>
+            <TextoExpansible contenido={textAUX} longitudMaxima={80}/>
+        </div>
+    </div>
+    )
+  }else if (data)
   return (
     <div>
         <div className="title">
@@ -30,19 +60,42 @@ const Figure = ({actualDate, data, handleNewDate, handleNewSelectOption}) => {
           </div>
             <div className='input'>
                 <label className='date_text'>Pick a date: </label>
-                <input type="date" id="date" name="date" max={today} value={actualDate} onChange={handleNewDate} />
+                <input type="date" id="date" name="date" min="1995-06-20" max={today} value={actualDate} onChange={handleNewDate} />
             </div>
             <div className="image_info">
                 <h2>Pic of the day: {data.date}</h2>
                 <p className='author'>Title: {data.title}</p>
             </div>
             <div className="element-image">
-                <img className='nasa_photo' src={data.url} alt="Image of the day"></img>
+                <img className='nasa_photo' src={data.url} alt="APOD- Image of the day"></img>
             </div>
             <TextoExpansible contenido={data.explanation} longitudMaxima={80}/>
         </div>
     </div>
   )
+  else 
+    return (
+    <div>
+        <div className="title">
+            <h1>Astronomic image of the day</h1>
+        </div>
+        <div className="info_container">
+          <div className="select_container">
+          <select className='select' onChange={handleNewSelectOption}>
+            <option value="nasa" defaultValue>NASA</option>
+            <option value="mars-rover-photo">Mars Rover Photo</option>
+          </select>
+          </div>
+            <div className='input'>
+                <label className='date_text'>Pick a date: </label>
+                <input type="date" id="date" name="date" min="1995-06-20" max={today} value={actualDate} onChange={handleNewDate} />
+            </div>
+            <div className="element-image">
+                <img className='nasa_photo' src="#" alt="Image not available now."></img>
+            </div>
+        </div>
+    </div>
+    )
 }
 
 export default Figure
